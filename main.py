a03a4e071824bcaa1e6a62f2c064d19351c94ae2
PicciHimu <101346950+Foyezrabbi@users.noreply.github.com>
2022-09-01 14:27:01 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import time
-from selenium import webdriver
 import random
+from selenium import webdriver
 
 print("https://chromedriver.chromium.org/downloads")
 url = input("Enter The URL Below: ")  # 'https://www.neoenbd.com/#/register?inviteCode=871424'
@@ -9,8 +9,8 @@ acc_num = int(input("How many account you want to generate : "))
 for i in range(acc_num):
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("--incognito")
+    
     # driver = webdriver.Chrome(executable_path="<path of chrome_driver.exe file>",options=chrome_options)
-
     driver = webdriver.Chrome(executable_path="C:\\Users\\Himu\\Downloads\\chromedriver_win32\\chromedriver.exe",options=chrome_options)
     time.sleep(1)
     driver.get(url)
@@ -28,7 +28,10 @@ for i in range(acc_num):
         driver.find_element_by_xpath("//*[@id='app']/div[1]/div[3]/div[1]/div[2]/div[2]/div/input").send_keys(pass_gen)
         driver.find_element_by_xpath("//*[@id='app']/div[1]/div[3]/div[1]/div[3]/div[2]/div/input").send_keys(pass_gen)
         create = driver.find_element_by_xpath("//*[@id='app']/div[1]/div[3]/button").click()
+        
         print(num_gen)
         num_list.append(num_gen)
         driver.close()
         time.sleep(3)
+        
+print("done")
